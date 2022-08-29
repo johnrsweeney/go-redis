ea6e493ab5a5ca0829c756263930a8608e68d20c
Cloud User <centos@ip-172-31-22-11.eu-west-2.compute.internal>
2022-08-29 01:30:08 +0000

    Add setstatus endpoint

--- main.go
+++ main.go
@@ -7,23 +7,6 @@ import (
 	"github.com/go-redis/redis"
 )
 
-type album struct {
-	ID		string	`json:"id"`
-	Title	string	`json:"title"`
-	Artist	string	`json:"artist"`
-	Price	float64	`json:"price"`
-}
-
-var albums = []album {
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
-}
-
 func getStatus(c *gin.Context) {
 	client := redis.NewClient(&redis.Options{
 		Addr: "0.0.0.0:6379",
@@ -31,20 +14,36 @@ func getStatus(c *gin.Context) {
 		DB: 0,
 	})
 
-	name, err := client.Get("name").Result()
+	status, err := client.Get("status").Result()
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	c.JSON(http.StatusOK, gin.H {
-		"status": name,
+		"status": status,
 	})
+}
+
+func setStatus(c *gin.Context) {
+	client := redis.NewClient(&redis.Options{
+		Addr: "0.0.0.0:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	status_param := c.Request.URL.Query().Get("status");
+
+	err := client.Set("status", status_param, 0).Err();
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func main() {
 	router := gin.Default()
 //	router.SetTrustedProxies([]string{"172.0.0.1"})
 
+	router.GET("/setstatus", setStatus)
 	router.GET("/status", getStatus)
 
 	router.Run(":80")
